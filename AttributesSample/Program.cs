d0ec95c2fb32ae22794b4a63e6fae29325940a95
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-02-19 18:36:53 +0900

    Initial Commit

--- AttributesSample/Program.cs
+++ AttributesSample/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Reflection;
 
 namespace AttributesSample
 {
-    class Program
+    [AttributeUsage(
+        AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method,
+        AllowMultiple = true,
+        Inherited = false)]
+    public class AuthorAttribute : Attribute
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        private string name;
+        public AuthorAttribute(string name) { this.name = name; }
+        public string Name { get { return this.name;  } }
     }
+
+    [Author("Tsuyoshi Ushio")]
+    [Author("Kanio Dimitrov")]
+    class AuthorTest
+    {
+        [Author("Jimmy Page")]
+        public static void A() { }
+        [Author("Robert Plant")]
+        public static void B() { }
+        [Author("Jhon Paul Jones")]
+        public static void C() { }
+        [Author("Jhon Bohnam")]
+        public static void D() { }
+    }
+
+    class AttributeTest
+    {
+        static void Main()
+        {
+            GetAllAuthors(typeof(AuthorTest));
+            Console.ReadLine();
+        }
+
+        static void GetAllAuthors(Type t)
+        {
+            Console.Write("type name: {0}\n", t.Name);
+            GetAuthors(t);
+
+            foreach(MethodInfo info in t.GetMethods())
+            {
+                Console.Write(" method name: {0}\n", info.Name);
+                GetAuthors(info);
+            }
+        }
+
+        static void GetAuthors(MemberInfo info)
+        {
+            Attribute[] authors = Attribute.GetCustomAttributes(
+                info, typeof(AuthorAttribute));
+            foreach(Attribute att in authors)
+            {
+                AuthorAttribute author = att as AuthorAttribute;
+                if (author != null)
+                {
+                    Console.Write("  author name: {0}\n", author.Name);
+
+                }
+            }
+        }
+
+    }
+
 }
---
+++ GHDetector/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+
+namespace GHDetector
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method,
+        AllowMultiple = true, Inherited = false)]
+    public class GHDetectAttribute : Attribute
+    {
+        private string maker;
+        private bool isWig;
+        public GHDetectAttribute(bool isWig, string maker)
+        {
+            this.maker = maker;
+            this.isWig = isWig;
+        }
+        public string Maker {  get { return this.maker; } }
+        public bool IsWig { get { return this.isWig; } }
+    }
+
+    public interface Accused
+    {
+        void Execute();
+    }
+
+    [GHDetect(true, "アデランス")]
+    public class Takeru : Accused
+    {
+        public void Execute()
+        {
+            Console.WriteLine("[Takeru] 私ははげていません。");
+        }
+    }
+    [GHDetect(false, null)]
+    public class Tsuyoshi : Accused
+    {
+        public void Execute()
+        {
+            Console.WriteLine("[Tsuyoshi] 私ははげていません。");
+        }
+    }
+
+
+    class Program
+    {
+
+        static Attribute DetectGH(MemberInfo info)
+        {
+            Attribute detector = Attribute.GetCustomAttribute(
+                info, typeof(GHDetectAttribute));
+            return detector;
+        }
+
+        static void Main(string[] args)
+        {
+            Accused tsuyoshi = new Tsuyoshi();
+            Execute(tsuyoshi);
+            Console.ReadLine();
+            Accused takeru = new Takeru();
+            Execute(takeru);
+            Console.ReadLine();
+            
+        }
+
+        static void Execute(Accused accused)
+        {
+            var accusedAttribute = DetectGH(accused.GetType()) as GHDetectAttribute;
+            if (accusedAttribute.IsWig)
+            {
+                accused.Execute();
+                Console.WriteLine($"カツラが検出されました。　メーカー: {accusedAttribute.Maker}");
+            } else
+            {
+                accused.Execute();
+                Console.WriteLine("本当に地毛のようです。");
+            }
+        }
+    }
+}
